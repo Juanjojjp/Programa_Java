1833d74c6d3efbd50e177c039505d94309a12b26
Juanjojjp <89483602+Juanjojjp@users.noreply.github.com>
2021-10-31 22:00:20 -0500

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -1,3 +1,11 @@
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+class Programa {
+    public static void main(String[] args) {
+        // Inicio del codigo
+        // Apertura de Scanner para solicitar inputs
+        Scanner datosEntrada = new Scanner(System.in);
+        Scanner datosExtras = new Scanner(System.in);
+        
